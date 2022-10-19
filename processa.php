6c41466c24de6c28ad8102bf35fbf9bba877d990
Luiiizzspk <brluizhd@gmail.com>
2022-10-19 08:45:16 -0300

    update

---
+++ processa.php
@@ -0,0 +1,19 @@
+<?php
+
+//Recebe os campos do formulário
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$sexo = $_POST['sexo'];
+$telefone = $_POST['telefone'];
+$senha = $_POST['senha'];
+$idade = $_POST['idade'];
+$estado = $_POST['estado'];
+$cidade = $_POST['cidade'];
+$destinos = $_POST['destinos'];
+$hospedagem = $_POST['hospedagem'];
+$mensagem = $_POST['mensagem'];
+$dt_cadastro = $_POST['dt-cadastro'];
+
+
+//para investigar variáveis e expressões
+/* var_dump($_POST); */
